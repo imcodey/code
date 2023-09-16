e542fa66a1a9975bf2d94755ad348b23cf8fe4cf
Mukesh Yaduvanshi <143539631+imcodey@users.noreply.github.com>
2023-09-16 07:31:14 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+// let blink = document.getElementById("blink")
+
+// setInterval(()=>{
+    
+// },5000)
+
+let typed = new Typed('#element',{
+    strings:['Hey',`I'm`,'Mukesh Yaduvanshi','Please Pura Dekhna','I want to say','Sorry','मुझे नहीं पता लेकिन तुमने कुछ दिनों से बात करना ही बंद कर दिया है...','मुझे लगता है इसमें कहीं ना कहीं मेरी गलती, मेरी ही होगी वरना क्यू ही कोई मुझसे वो भी अचानक से बात करना बंद कर देगा..','अगर लगता है गलती मेरी है तो मुझे बता दो..','एक बार बात कर के तो देखो क्या पता सारी समस्या ही ख़तम हो जाये..','Please Ek baar baat kar lo'],
+    typeSpeed:90,
+    backSpeed:20,
+    loop:false
+});
